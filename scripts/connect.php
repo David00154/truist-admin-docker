b0f4996cd7f13b8c3293d523b7ea747ac9ebd2d6
David Briggs <davidbriggs00154@gmail.com>
2023-12-01 00:51:10 +0100

    Update connect.php

--- scripts/connect.php
+++ scripts/connect.php
@@ -1,8 +1,14 @@
 <?php
-$servername = "bigswfmt9xdwwxeosjaa-mysql.services.clever-cloud.com";
-$username = "uyjojewbw23wwx47";
-$password = "W06QhyULqIFFczwaql4w";
-$dbname = "bigswfmt9xdwwxeosjaa";
+$servername = "s10.my-control-panel.com";
+$username = "elitepri_truist";
+$dbname = "elitepri_truist";
+$password = "admin@00154";
+
+
+// $servername = "bigswfmt9xdwwxeosjaa-mysql.services.clever-cloud.com";
+// $username = "uyjojewbw23wwx47";
+// $password = "W06QhyULqIFFczwaql4w";
+// $dbname = "bigswfmt9xdwwxeosjaa";
 
 // Create connection
 $conn = @new mysqli($servername, $username, $password, $dbname);
